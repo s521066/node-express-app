420285903c67608600436585c45756b2839fb5c1
Chulho Yoon <ychidra@gmail.com>
2019-03-07 11:12:29 -0600

    console.log GET and POST requests

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express')
 const app = express()
 
-const hostname = '0.0.0.0'    // allows access from remote computers
+const hostname = '0.0.0.0'   // set constants
 const port = 3002
 
-app.get('/', function (req, res) {
+app.post('/', function (req, res) {
+  console.log('POST request')
   res.send('Welcome home!')
 })
 
+app.use(function (req, res, next) {
+  console.log('logged')
+  next()
+})
+
 app.get('/hello', (req, res) => {
+  console.log('GET request')
   res.send('Hello World!')
 })
 
 app.get('/big',  (req, res) =>{
+  console.log('GET request')
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/greeting/:id',  (req, res) =>{
+  console.log('GET request')
   res.send('Hello! The id was ' + req.params.id)
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
+  console.log('GET request')
   res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
 })
 
